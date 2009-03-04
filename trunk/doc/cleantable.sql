95c96cfe690711e48e2155cdcb492909053fa1d7
novthirteen <novthirteen@1ac4a774-0534-11de-a6e9-d320b29efae0>
2009-03-04 06:08:28 +0000

    git-svn-id: https://emma-oa.googlecode.com/svn@13 1ac4a774-0534-11de-a6e9-d320b29efae0

--- trunk/doc/cleantable.sql
+++ trunk/doc/cleantable.sql
@@ -2,59 +2,65 @@ delete from expense_cell_det;
 delete from expense_row_det;
 delete from expense_attach;
 delete from expense_claim;
-delete from exp_cell_det_hist
-delete from exp_row_det_hist
-delete from expense_approver
-delete from expense_recharge
-delete from expense_history
-delete from expense
+delete from exp_cell_det_hist;
+delete from exp_row_det_hist;
+delete from expense_approver;
+delete from expense_recharge;
+delete from exp_cell_det_hist;
+delete from exp_row_det_hist;
+delete from expense_history;
+delete from expense;
 
-delete from po_attach
-delete from po_approver
-delete from po_item_cancel
-delete from po_item_history
+delete from pr_item_history;
+delete from pr_history;
+delete from po_attach;
+delete from po_approver;
+delete from po_item_cancel;
+delete from po_item_history;
+delete from po_item_receipt;
+delete from po_item_recharge;
+delete from po_history;
+update pr_item set po_item_id = null;
 delete from po_item_receipt
-delete from po_item_recharge
-delete from po_history
-update pr_item set po_item_id = null
-delete from po_item
-delete from purchase_order
+delete from air_ticket;
+delete from po_item;
+delete from purchase_order;
 
-delete from pr_item_history
-delete from pr_item_attach
-delete from pr_item_recharge
-delete from pr_item
-delete from pr_approver
-delete from pr_attach
-delete from pr_authorized_purchaser
-delete from pr_history
-delete from purchase_request
+delete from pr_item_history;
+delete from pr_item_attach;
+delete from pr_item_recharge;
+delete from pr_item;
+delete from pr_approver;
+delete from pr_attach;
+delete from pr_authorized_purchaser;
+delete from purchase_request;
 
-delete from ta_approver
-delete from ta_history
-delete from air_ticket_recharge
-delete from air_ticket
-delete from travel_application
+update expense set ta_no = null;
+delete from ta_approver;
+delete from ta_history;
+delete from air_ticket_recharge;
+delete from air_ticket;
+delete from travel_application;
 
-delete from capex_req_item_history
-delete from  capex_request_history
-delete from  capex_request_item
-delete from  capex_request_approver
-delete from  capex_request_attach
-delete from  capex_department
-update capex set capex_req_id = null
-delete from capex_request
-delete from capex
+delete from capex_req_item_history;
+delete from  capex_request_history;
+delete from  capex_request_item;
+delete from  capex_request_approver;
+delete from  capex_request_attach;
+delete from  capex_department;
+update capex set capex_req_id = null;
+delete from capex_request;
+delete from capex;
 
-delete from email_batch
-delete from email
+delete from email_batch;
+delete from email;
 
-delete from approver_delegate
+delete from approver_delegate;
 
-delete from yearly_bgt_dep_history
-delete FROM yearly_budget_history
-delete FROM yearly_budget_department
-delete FROM yearly_budget
+delete from yearly_bgt_dep_history;
+delete FROM yearly_budget_history;
+delete FROM yearly_budget_department;
+delete FROM yearly_budget;
 
 delete from travel_group_det
 
